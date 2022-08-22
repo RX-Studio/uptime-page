16769c7ef1604e0728656b96b11005228005f20c
Nero978 <zhumengfeiyang@qq.com>
2022-08-23 00:00:30 +0800

    fix uptime api

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'RX工作室 - 服务状态',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur894355-a80b3ecfd3c74cd5d14728cd',
   ],
 
   // 日志天数
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '官网',
+      url: 'https://www.rxgzs.cn/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/RX-Studio'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://blog.rxgzs.cn/'
     },
   ],
 };
